6ca9880e4c53756ab204450ee62bc3f1fb9e5192
Jason <124936478+RoboRaccoon@users.noreply.github.com>
2024-02-24 16:53:39 -0500

    Forgot to initialize database and runserver (the placeholder image now works)

--- HFH_project/HFH_app/views.py
+++ HFH_project/HFH_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'home_page/index.html')
+
+#buttons don'e work yet
+def login(request):
+    return (render, 'home_page/index.html')
+
+#request to make new account
+def get_started(request):
+    return render(request, 'home_page/index.html')
