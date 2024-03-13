93a38aa05fad1bd5e5e765e032c81558807781de
Jayne Davies <jaynedavies@cmail.carleton.ca>
2024-03-13 11:13:34 -0400

    adding other sites
    this is in progress

--- HFH_project/HFH_app/views.py
+++ HFH_project/HFH_app/views.py
@@ -4,9 +4,33 @@ from django.shortcuts import render
 def main(request):
     return render(request, 'home_page/index.html')
 
-#buttons doesn't work yet
+#------------------------------------------------------------------------------------------
+def about_us(request):
+    return render(request, "info_pages/about_us.html")
+def contact_us(request):
+    return render(request, "info_pages/contact_us.html")
+#------------------------------------------------------------------------------------------
+
+def calorie_counter(request):
+    return render(request, "feature_pages/calorie-counter.html")
+#------------------------------------------------------------------------------------------
+
+def profile(request):
+    return render(request, "Login_pages/profile.html")
 def login(request):
-    return (render, 'home_page/index.html')
+    return render(request, 'Login_pages/login.html')
+def sign_up(request):
+  return render(request, 'Login_pages/sign_up.html')
+#------------------------------------------------------------------------------------------
+def privacy_policy(request):
+  return render(request, 'legal_pages/privacy_policy.html')
+def terms_of_service(request):
+  return render(request, 'legal_pages/terms_of_service.html')
+#------------------------------------------------------------------------------------------
+
+
+#buttons doesn't work yet
+
 
 #request to make new account
 def get_started(request):
