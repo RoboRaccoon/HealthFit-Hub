2d433432631af6f62e750d3a6749805358bdc341
Jason <124936478+RoboRaccoon@users.noreply.github.com>
2024-03-18 04:46:16 -0400

    calorie counter update
    we have a list of foods in our database in html

--- HFH_project/HFH_app/views.py
+++ HFH_project/HFH_app/views.py
@@ -1,46 +1,50 @@
 from django.shortcuts import render
+from . import models
+
 
 # Create your views here.
 def main(request):
     return render(request, 'home_page/index.html')
 
 
-
-
 def about_us(request):
     return render(request, "info-pages/about-us.html")
+
+
 def contact_us(request):
     return render(request, "info-pages/contact_us.html")
+
+
 def help(request):
     return render(request, "info-pages/help.html")
 
 
-
-
 def calorie_counter(request):
-    return render(request, "feature_pages/calorie-counter.html")
-
-
+    retrieved_food_items = models.Food.objects.all()
+    return render(request, "feature_pages/calorie-counter.html", {'food_items': retrieved_food_items})
 
 
 def profile(request):
     return render(request, "Login_pages/profile.html")
+
+
 def login(request):
     return render(request, 'Login_pages/login.html')
-def sign_up(request):
-  return render(request, 'Login_pages/sign_up.html')
 
+
+def sign_up(request):
+    return render(request, 'Login_pages/sign_up.html')
 
 
 def privacy_policy(request):
-  return render(request, 'legal_pages/privacy_policy.html')
+    return render(request, 'legal_pages/privacy_policy.html')
+
+
 def terms_of_service(request):
-  return render(request, 'legal_pages/terms_of_service.html')
+    return render(request, 'legal_pages/terms_of_service.html')
 
 
-#buttons doesn't work yet
-
-
-#request to make new account
+# buttons doesn't work yet
+# request to make new account
 def get_started(request):
     return render(request, 'home_page/index.html')
