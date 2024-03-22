1c80c063fc517d79dd8f0472a7959ed146b7e893
Jason <124936478+RoboRaccoon@users.noreply.github.com>
2024-03-22 03:07:38 -0400

    added food groups
    the food groups will help us with filtering

--- HFH_project/HFH_app/models.py
+++ HFH_project/HFH_app/models.py
@@ -4,8 +4,30 @@ from django.db import models
 
 
 class Food(models.Model):
+    # 1st key = set on model, 2nd key = human readable
+    FOOD_GROUPS = {
+        "Baked Foods": "Baked Foods",
+        "Snacks": "Snacks",
+        "Sweets": "Sweets",
+        "Vegetables": "Vegetables",
+        "American Indian": "American Indian",
+        "Restaurant Foods": "Restaurant Foods",
+        "Beverages": "Beverages",
+        "Fats and Oils": "Fats and Oils",
+        "Meats": "Meats",
+        "Dairy and Egg Products": "Dairy and Egg Products",
+        "Baby Foods": "Baby Foods",
+        "Breakfast Cereals": "Breakfast Cereals",
+        "Soups and Sauces": "Soups and Sauces",
+        "Beans and Lentils": "Beans and Lentils",
+        "Fish": "Fish",
+        "Fruits": "Fruits",
+
+    }
+
     name = models.CharField(max_length=200)
     calories = models.IntegerField(default=0)
+    foodGroups = models.CharField(max_length=50, choices=FOOD_GROUPS, default='Baked Foods')
 
     def __str__(self):
         return self.name
@@ -17,5 +39,3 @@ class FoodsEatenList(models.Model):
 
     def __str__(self):
         return self.title
-
-
